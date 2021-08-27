636a8d7ff1bc265ad8bc7da3d868e0cc9eb73ea1
BlakeMcD <blakemcd@gmail.com>
2021-08-27 13:26:51 +1000

    still cannot figure it out

--- src/components/Favourites.js
+++ src/components/Favourites.js
@@ -1,14 +1,21 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
+import Movie from './Movie'
+
+export const FavouritesContext2 = React.createContext()
 
 function Favourites() {
 
-    // export const FavouritesContext = 
 
     const [favourite, setFavourite] = useState([])
+    const [test, setTest] = useState('componentTest')
 
     return (
         <div>
-            This is the favourites section
+            <h2>Favourites</h2>
+            <p>This is the favourites section</p>        
+            {/* <FavouritesContext.Provider>
+                <Movie/>
+            </FavouritesContext.Provider> */}
         </div>
     )
 }
--- src/components/Navigation/Home.js
+++ src/components/Navigation/Home.js
@@ -1,22 +1,27 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import Title from '../Title'
 import Favourites from '../Favourites'
 import Search from '../SearchOriginal'
 import TrendingMovies from '../TrendingMovies'
 
+export const FavouritesContext = React.createContext();
 
 function Home() {
+
+    const [faveMovie, setFaveMovie] = useState("Legend of the legends")
+
     return (
         <div>
-            <Title/>
-            <Favourites/>
-            <Search/>
-            <TrendingMovies/>
-            <h2>THIS IS THE HOME PAGE
+            <FavouritesContext.Provider value={faveMovie}>
+                <Title/>
+                <Favourites/>
+                <Search/>
+                <TrendingMovies/>
+                <h2>THIS IS THE HOME PAGE
 
-            Why can't I see anything??????
-            </h2>
-
+                Why can't I see anything??????
+                </h2>
+            </FavouritesContext.Provider>
         </div>
     )
 }
