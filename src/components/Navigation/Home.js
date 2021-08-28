8587f6c71a43c7ff91124ad7b107f950876825bd
BlakeMcD <blakemcd@gmail.com>
2021-08-28 10:49:43 +1000

    made posters smaller and also added new sections and did some css stuff

--- src/components/Navigation/Home.js
+++ src/components/Navigation/Home.js
@@ -3,6 +3,10 @@ import Title from '../Title'
 import Favourites from '../Favourites'
 import Search from '../Search'
 import TrendingMovies from '../TrendingMovies'
+import GenreDrama from '../GenreDrama'
+import GenreComedy from '../GenreComedy'
+import GenreThriller from '../GenreThriller'
+import GenreAnimation from '../GenreAnimation' 
 
 export const FavouritesContext = React.createContext();
 
@@ -13,10 +17,14 @@ function Home() {
     return (
         <div className="bodyContentUnderNav">
             <FavouritesContext.Provider value={{faveMovie, setFaveMovie}}>
-                <Title/>
+                {/* <Title/> */}
                 <Favourites/>
                 <Search/>
                 <TrendingMovies/>
+                <GenreDrama/>
+                <GenreComedy/>
+                <GenreThriller/>
+                <GenreAnimation/>
             </FavouritesContext.Provider>
         </div>
     )
